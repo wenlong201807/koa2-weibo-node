eb650a6a802b8d875488ff0182b1b437ff35ec84
wenlongmac <2854588836@qq.com>
2022-02-01 15:21:34 +0800

    utli

---
+++ src/db/sync.js
@@ -0,0 +1,25 @@
+/**
+ * @description sequelize 同步数据库
+ * @author wenlong
+ */
+
+const seq = require('./seq')
+
+require('./model/index')
+
+// 测试连接
+seq
+  .authenticate()
+  .then(() => {
+    console.log('auth ok')
+  })
+  .catch(() => {
+    console.log('auth err')
+  })
+
+// 执行同步
+seq.sync({ force: true }).then(() => {
+  // force: true 删掉原来的数据库，重新建立
+  console.log('sync ok')
+  process.exit()
+})
