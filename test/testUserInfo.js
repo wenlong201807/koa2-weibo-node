48a7d0036950d6cb4fa9d08598505ba68d0c332c
wenlongmac <2854588836@qq.com>
2022-02-02 23:26:19 +0800

    feat:创建博客

--- test/testUserInfo.js
+++ test/testUserInfo.js
@@ -5,15 +5,15 @@
 
 /**
  * 【特别提醒】cookie 是用户的敏感信息，此处只能是**测试**用户的 cookie
- * 每次测试用户重新登录，都需要更新这里的 cookie
+ * 每次测试用户重新登录，都需要更新这里的 cookie // 从浏览器的cookie中获取最新的
  */
 
 module.exports = {
     Z_ID: 1,
     Z_USER_NAME: 'zhangsan',
-    Z_COOKIE: 'weibo.sid=8YVHHiofAiPqNvHHt91Rw7SZaXOfivab; weibo.sid.sig=q_gnrP-vM1ywHaNpZSlHst1XmgQ',
+    Z_COOKIE: 'weibo.sid=GVLtgPb9NEUGioTqHqgNF2Djr7Y-6TFs; weibo.sid.sig=HTSmDI5JmSGYecrDI6fYb0qRTmk',
 
-    L_ID: 2,
-    L_USER_NAME: 'lisi',
-    L_COOKIE: 'weibo.sid=686m0u92zMTCmmSQY85trYv_PScDgpFx; weibo.sid.sig=Za_Oj-wSLFrG5QzQhqQU0YQxOFo'
+    // L_ID: 2,
+    // L_USER_NAME: 'lisi',
+    // L_COOKIE: 'weibo.sid=686m0u92zMTCmmSQY85trYv_PScDgpFx; weibo.sid.sig=Za_Oj-wSLFrG5QzQhqQU0YQxOFo'
 }
