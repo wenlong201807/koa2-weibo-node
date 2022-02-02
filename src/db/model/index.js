62a6138a5724acd2e5b302aef3dcedb01cd33b51
wenlongmac <2854588836@qq.com>
2022-02-02 22:16:23 +0800

    feat:微博用户模型

--- src/db/model/index.js
+++ src/db/model/index.js
@@ -4,10 +4,12 @@
  */
 
 const User = require('./User')
-const Blog = require('./Blog')
+const Blog = require('./Blog') // 每次新增一个数据库表，都需要重新导入到数据库中，需要执行 node src/db/sync.js
 const UserRelation = require('./UserRelation')
 const AtRelation = require('./AtRelation')
 
+// 外键关联:一个用户可以有多条微博内容
+// 查用户的时候查询出对应的微博
 Blog.belongsTo(User, {
   foreignKey: 'userId',
 })
@@ -15,6 +17,7 @@ Blog.belongsTo(User, {
 UserRelation.belongsTo(User, {
   foreignKey: 'followerId',
 })
+
 User.hasMany(UserRelation, {
   foreignKey: 'userId',
 })
