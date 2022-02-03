3cf858ba40273ffd928ce16b4d1aa4cf27135766
wenlongmac <2854588836@qq.com>
2022-02-03 22:01:39 +0800

    feat:样式微调

--- src/conf/constant.js
+++ src/conf/constant.js
@@ -4,7 +4,8 @@
  */
 
 module.exports = {
-  DEFAULT_PICTURE: 'https://dwz.cn/rnTnftZs', // 用户默认头像
+  // DEFAULT_PICTURE: 'https://dwz.cn/rnTnftZs', // 用户默认头像
+  DEFAULT_PICTURE: 'https://ss2.bdstatic.com/70cFvnSh_Q1YnxGkpoWK1HF6hhy/it/u=550723927,1346838877&fm=27&gp=0.jpg', // 用户默认头像
   PAGE_SIZE: 5,
 
   // 正则表达式，匹配 '@昵称 - userName'
